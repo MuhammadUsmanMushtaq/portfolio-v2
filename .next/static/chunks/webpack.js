29cf9a2643026773ec231c7eff989ac46e06a1b3
Muhammad Usman <usman820901@gmail.com>
2023-07-20 23:47:01 +0200

    next-event image updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b5832fe184a29d2"; }
+/******/ 		__webpack_require__.h = function() { return "911ebd3766e1ace3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
