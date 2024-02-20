2f8c496f5b8b3ba8a47f52f54eef9f1ce197b7f5
Muhammad Usman <usman820901@gmail.com>
2024-02-20 11:32:35 +0100

    project update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "911ebd3766e1ace3"; }
+/******/ 		__webpack_require__.h = function() { return "51a81ad05a3c93ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
